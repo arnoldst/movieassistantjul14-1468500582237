ed10ec1a603e85f128a5c8c84245dff6625c1f3e
Steve Arnold <steve.arnold@uk.ibm.com>
2016-07-14 07:54:37 -0500

    api key added

--- api/services.js
+++ api/services.js
@@ -26,7 +26,7 @@ var classifierFile = __dirname + '/../training/classifier_id';
 // This application requires 3 ids to work properly
 
 // 1. TMDB API key, explained in https://www.themoviedb.org/documentation/api
-var TMDB_API_KEY = process.env.TMDB_API_KEY || '';
+var TMDB_API_KEY = process.env.TMDB_API_KEY || '16a22bc1e1a6d89755c8538c58c53e1d';
 
 // 2. dialog id - see training/setup.js
 var DIALOG_ID = 'TYPE DIALOG ID HERE';
